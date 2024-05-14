4df549ad81ce01efef4f0d3394cfe3487c330837
Dave Stewart <dev@davestewart.co.uk>
2024-05-14 23:06:25 +0100

    Complete initial migration of core concerns

--- core/nuxt.config.ts
+++ core/nuxt.config.ts
@@ -1,3 +1,83 @@
+import { defu } from 'defu'
+import { createResolver } from '@nuxt/kit'
+import { defineNuxtConfig } from 'nuxt/config'
+
+const { resolve } = createResolver(import.meta.url)
+
+// debug
 console.log('Adding layer: core')
 
-export default defineNuxtConfig({})
+// That allows to overwrite these dependencies paths via `.env` for local development
+const envModules = {
+  tokens: process?.env?.THEME_DEV_TOKENS_PATH || '@nuxt-themes/tokens',
+  elements: process?.env?.THEME_DEV_ELEMENTS_PATH || '@nuxt-themes/elements',
+  studio: process?.env?.THEME_DEV_STUDIO_PATH || '@nuxthq/studio',
+  typography: process?.env?.THEME_DEV_TYPOGRAPHY_PATH || '@nuxt-themes/typography'
+}
+
+const config = defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      FORMSPREE_URL: process.env.FORMSPREE_URL
+    }
+  },
+})
+
+const modules = defineNuxtConfig({
+  extends: [
+    envModules.typography,
+    envModules.elements,
+  ],
+
+  modules: [
+    envModules.tokens,
+    envModules.studio,
+    '@nuxtjs/plausible',
+    '@nuxt/devtools'
+  ],
+
+  pinceau: {
+    studio: true
+  },
+
+  nitro: {
+    prerender: {
+      ignore: ['/__pinceau_tokens_config.json', '/__pinceau_tokens_schema.json']
+    }
+  },
+})
+
+const build = defineNuxtConfig({
+  experimental: {
+    inlineSSRStyles: false
+  },
+
+  typescript: {
+    includeWorkspace: true
+  },
+})
+
+const app = defineNuxtConfig({
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en'
+      }
+    }
+  },
+
+  colorMode: {
+    classSuffix: ''
+  },
+
+  css: [
+    resolve('./assets/main.css'),
+  ],
+})
+
+export default defu(
+  config,
+  modules,
+  build,
+  app
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,47 +2,16 @@ import { createResolver } from '@nuxt/kit'
 
 const { resolve } = createResolver(import.meta.url)
 
-// That allows to overwrite these dependencies paths via `.env` for local development
-const envModules = {
-  tokens: process?.env?.THEME_DEV_TOKENS_PATH || '@nuxt-themes/tokens',
-  elements: process?.env?.THEME_DEV_ELEMENTS_PATH || '@nuxt-themes/elements',
-  studio: process?.env?.THEME_DEV_STUDIO_PATH || '@nuxthq/studio',
-  typography: process?.env?.THEME_DEV_TYPOGRAPHY_PATH || '@nuxt-themes/typography'
-}
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   extends: [
-    // third-party layers
-    envModules.typography,
-    envModules.elements,
-
-    // local layers
     './core',
   ],
-
-  app: {
-    head: {
-      htmlAttrs: {
-        lang: 'en'
-      }
-    }
-  },
-
-  runtimeConfig: {
-    public: {
-      FORMSPREE_URL: process.env.FORMSPREE_URL
-    }
-  },
 
   pages: true,
 
   modules: [
-    envModules.tokens,
-    envModules.studio,
     '@nuxt/content',
-    '@nuxtjs/plausible',
-    '@nuxt/devtools'
   ],
 
   components: [
@@ -50,18 +19,6 @@ export default defineNuxtConfig({
     { path: resolve('./components/content'), global: true },
     { path: resolve('./components/data-entry'), global: true }
   ],
-
-  css: [
-    resolve('./assets/main.css'),
-  ],
-
-  colorMode: {
-    classSuffix: ''
-  },
-
-  pinceau: {
-    studio: true
-  },
 
   content: {
     documentDriven: true,
@@ -74,20 +31,6 @@ export default defineNuxtConfig({
         dark: 'github-dark'
       },
       preload: ['json', 'js', 'ts', 'html', 'css', 'vue', 'diff', 'shell', 'markdown', 'yaml', 'bash', 'ini', 'c', 'cpp']
-    }
-  },
-
-  experimental: {
-    inlineSSRStyles: false
-  },
-
-  typescript: {
-    includeWorkspace: true
-  },
-
-  nitro: {
-    prerender: {
-      ignore: ['/__pinceau_tokens_config.json', '/__pinceau_tokens_schema.json']
     }
   },
 })

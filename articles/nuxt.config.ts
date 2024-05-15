c0fa67944df9ebbde2dd4565549f4d3615d4cfa7
Dave Stewart <dev@davestewart.co.uk>
2024-05-15 17:01:49 +0100

    Move initial article concerns

--- articles/nuxt.config.ts
+++ articles/nuxt.config.ts
@@ -1,4 +1,9 @@
 // debug
 console.log('Adding layer: articles')
 
-export default defineNuxtConfig({})
+export default defineNuxtConfig({
+  components: [
+    // mark components as global so they work in content
+    { path: 'components', global: true },
+  ]
+})
